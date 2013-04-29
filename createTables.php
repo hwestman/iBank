29290c48efc6d6e6eb1f501d5444c98069901f46
Lance N. Solomon <lance.solomon@griffithuni.edu.au>
2013-04-29 14:32:52 +1000

    blah

---
+++ createTables.php
@@ -0,0 +1,21 @@
+<?php
+/*
+function connect()
+{
+	$u = 's2776885';
+	$p = 'shanaye';
+	$localhost = 'localhost';
+	$connection = oci_connect($u, $p, $localhost);
+}
+
+function disconnect()
+{
+	if($connection)
+	{
+	oci_close($connection);
+	}
+}
+*/
+
+
+?>
