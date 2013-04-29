d19a8ed4f0122866ebfb4df9c53bdfbba41379e7
Hallvard <hallvard@westman.no>
2013-04-29 17:03:09 +1000

    structure after seperation of sql-files

--- connection.php
+++ connection.php
@@ -3,10 +3,20 @@
 
 $dbs = "(DESCRIPTION = (ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = dwarf.cit.griffith.edu.au)(PORT = 1526)))(CONNECT_DATA =(SID = DBS)(SERVER = DEDICATED)))";
 $db = OCILogon("s2873575","dba",$dbs);
-$stmt = OCIParse($db,"select ENAME, FROM EMP2");
-OCI_Define_By_Name($stmt,"ENAME",$name);        
-$res = OCIExecute($stmt);
-
+$stmt = OCIParse($db,"select * from emp2");
+//OCI_Define_By_Name($stmt,"ENAME",$name);        
+OCIExecute($stmt);
+echo "test";
+echo "<table border='1'>\n";
+while ($row = oci_fetch_array($stmt, OCI_ASSOC+OCI_RETURN_NULLS)) {
+    echo "<tr>\n";
+    foreach ($row as $item) {
+        echo "    <td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
+    }
+    echo "</tr>\n";
+}
+echo "</table>\n";
+/*
 echo"<TABLE border=1 width=90%>";
 while(OCIFetch($stmt)){
 	echo "<TR>";
@@ -15,6 +25,7 @@ while(OCIFetch($stmt)){
         
 }
 echo"</TABLE>";
+*/
 OCIFreeStatement($stmt);
 OCILogoff($db);
  
