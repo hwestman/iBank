7068b13fcbd3e0b3e5d62296cd93dc8c3a33f802
Lance N. Solomon <lance.solomon@griffithuni.edu.au>
2013-05-08 23:46:45 +1000

    Login page

--- index.php
+++ index.php
@@ -1,37 +1,37 @@
 <?php
-    $i = "hallvard";
-    $j = 4;
-    $k = 6;
-    $l = $k+$j;
+    //this needs all required files
 
 ?>
 <!DOCTYPE html>
 <html dir="ltr" lang="en-US">
 
-<head>
-    <title>iBank</title>
-    <meta charset="UTF-8"/>
-    <link rel="stylesheet" type="text/css" href="/iBank/css/style.css" />
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
-     
-    
-</head>
-        <body>
-            <h1>iBank</h1>
-            <form name="credentialForm" id="credentialForm" action="#" method="post"> 
-		<label for="username">Username:</label> 
-		<input class="reqVal" type="text" name="username" id="username" required placeholder="Username" />
-                <i class="errorMessage"></i>
-                
-                <label for="password">Password:</label> 
-		<input class="reqVal" type="password" name="password" id="password" required placeholder="Password" />
-                <i class="errorMessage"></i>
-                
-                <input name="reset" type="button" value="Reset" />
-		<input name="submit" type="submit" value="Submit" disabled="disabled"/>
-            </form>
-            
-           <script type="text/javascript" src="/iBank/js/onload.js"></script>
-            
-        </body>
+	<head>
+	    <title>Mi Account - iBank</title>
+	    <meta charset="UTF-8"/>
+	    <link rel="stylesheet" type="text/css" href="css/style.css" />
+	    <link rel="stylesheet" type="text/css" href="css/menu.css" />
+	    <link rel="stylesheet" type="text/css" href="css/button.css" />
+	    <!--
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
+	    <script src="js/onload.js" type="text/javascript"></script>
+-->
+	</head>
+
+	<div id="container">
+		<?php include "include/header.php"; ?>
+		<div id="content">
+			<div id="content-main">
+				<table>
+					<th width="30%">Account number</th><th width="35%">Account type</th><th width="35%">Balance</th>
+					<tr bgcolor="#DDD"><td>12345678</td><td>Savings</td><td>$52,012.50</td></tr>
+					<tr bgcolor="#CCC"><td>12387654</td><td>Cheque</td><td>$10,012.50</td></tr>
+					<tr bgcolor="#DDD"><td>87654321</td><td>Credit</td><td>-$2,561.12</td></tr>
+					<tr bgcolor="#CCC"><td>81726354</td><td>Loan</td><td>-$250,321.87</td></tr>
+					<tr style="color:white; font-size:1.2em; font-weight:bold"><td></td><td>Total debt:</td><td>-$252,882.99</td></tr>
+					<tr style="color:white; font-size:1.2em; font-weight:bold"><td></td><td>Total credit:</td><td>$62,025.00</td></tr>
+				</table>
+			</div><!--CLOSE CONTENT MAIN-->
+		</div><!-- CLOSE CONTENT -->
+		<?php include "include/footer.php"; ?>
+	</div><!-- CLOSE CONTAINER -->
 </html>
--- login.php
+++ login.php
@@ -6,10 +6,15 @@
 <html dir="ltr" lang="en-US">
 
 	<head>
-	    <title>Login - iBank</title>
+	    <title>Mi Account - iBank</title>
 	    <meta charset="UTF-8"/>
 	    <link rel="stylesheet" type="text/css" href="css/style.css" />
-	    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
+	    <link rel="stylesheet" type="text/css" href="css/menu.css" />
+	    <link rel="stylesheet" type="text/css" href="css/button.css" />
+	    <!--
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
+	    <script src="js/onload.js" type="text/javascript"></script>
+-->
 	</head>
 
 	<div id="container">
@@ -18,13 +23,15 @@
 			<div class="login">
 				<form method="post" action="login().php" name="login">
 					<table>
-						<tr><td>User ID:</td><td><input name="username" id="username" type="text" size="30"/></td></tr>
-						<tr><td>Password:</td><td><input name="password" id="password" type="password" size="30"/></td></tr>
-						<tr><td><input type="submit" value="cancel" name="cancel"/></td><td style="float: right"><input type="submit" value="Login" name="login"/>
+						<tr><td>User ID:</td><td><input name="username" class="left" id="username" type="text" size="30"/></td></tr>
+						<tr><td>Password:</td><td><input name="password" class="left" id="password" type="password" size="30"/></td></tr>
 					</table>
+						<input type="submit" class="button" id="left" value="cancel" name="cancel"/>
+						<input type="submit" class="button" id="right" value="Login" name="login"/>
 				</form>
-			</div><!-- CLOSE LOGIN -->
+			</div><!--CLOSE LOGIN-->
 		</div><!-- CLOSE CONTENT -->
 		<?php include "include/footer.php"; ?>
 	</div><!-- CLOSE CONTAINER -->
 </html>
+
