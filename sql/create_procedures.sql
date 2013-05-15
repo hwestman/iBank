0a6fd38e704046c90a58a725a1f8ffbc28f40d52
hwestman <hwestman@10.0.0.4>
2013-05-15 15:49:34 +1000

    Adding connectiong and datastore back in, created indexes

--- sql/create_procedures.sql
+++ sql/create_procedures.sql
@@ -4,19 +4,32 @@
     sqlplus ibank_dba/billybob@dwarf @drop_tables.sql
     sqlplus ibank_dba/billybob@dwarf @structure.sql
     sqlplus ibank_dba/billybob@dwarf @sampleData.sql
-    sqlplus ibank_dba/billybob@dwarf @procedures.sql
+    sqlplus ibank_dba/billybob@dwarf @create_procedures.sql
     
     EXEC createAccount(1,4,'',2);
     
     SELECT createAccount(1,4,'',2) FROM DUAL;
     
+    DECLARE
+    BEGIN
+    END
+    
+    
+    
     	variable accountNumber NUMBER;
     	EXEC :accountNumber := createAccount(1,4,'',4);
     	PRINT accountNumber;
     	/
+    	
+    	variable receiptNumber NUMBER;
+    	EXEC :receiptNumber := transferFunds(18283004,18280330,'Enjoy',15000);
+    	PRINT receiptNumber;
+    	/
 */
 
 SET SQLBLANKLINES ON
+
+EXEC createUser('5','35 Serafina Drive','Queensland', 'Lance N. Solomon', 'shanaye', 4, '0421008826');
 
 CREATE OR REPLACE PROCEDURE createUser (
 	suburbID IN VARCHAR2,
@@ -25,7 +38,8 @@ CREATE OR REPLACE PROCEDURE createUser (
 	fullName IN VARCHAR2,  
 	pword IN VARCHAR2, 
 	priv IN INTEGER, 
-	contactNumber IN VARCHAR2
+	contactNumber IN VARCHAR2,
+	loginID OUT NUMBER
 )
 AS
 addressID NUMBER;
@@ -35,9 +49,15 @@ BEGIN
 		RETURNING address_id INTO addressID;
 	
 	INSERT INTO ibankUser
-		VALUES ('', fullName, addressID, pword, priv, contactNumber);
+		VALUES ('', fullName, addressID, pword, priv, contactNumber)
+		RETURNING login_id INTO loginID;
+	COMMIT;
 END;
 /
+select * from SYS.USER_ERRORS;
+
+
+
 
 CREATE OR REPLACE FUNCTION createAccount (
 	staffID IN NUMBER,
@@ -59,6 +79,48 @@ END;
 /
 
 
+CREATE OR REPLACE FUNCTION transferFunds (
+	accountFrom IN NUMBER,
+	accountTo IN NUMBER,
+	message IN VARCHAR2,
+	money IN NUMBER	
+)
+RETURN NUMBER
+AS
+fromAmount NUMBER;
+receiptNumber NUMBER;
+BEGIN	
+		SELECT balance FROM ibankAccount WHERE accountFrom = account_number;
+		RETURNING balance INTO fromAmount;
+
+		fromAmount := fromAmount - money;
+		
+		IF fromAmount < 0 THEN
+			RETURN 0;
+		ELSE
+			INSERT INTO ibankTransaction
+				(transaction_id, from_account, to_account, memo, amount)
+				VALUES ('', accountFrom, accountTo, message, money)
+				RETURNING transaction_id INTO receiptNumber;
+			
+			UPDATE ibankAccount
+				SET balance = balance - money WHERE accountFrom = ibankAccount.account_number;
+			
+			UPDATE ibankAccount
+				SET balance = balance + money WHERE accountTo = ibankAccount.account_number;
+			
+			IF receiptNumber > 0 THEN
+				COMMIT;
+				RETURN receiptNumber;
+			ELSE
+				ROLLBACK;
+				RETURN 0;
+			END IF;
+		END IF;
+END;
+/
+
+select * from SYS.USER_ERRORS;
 
 /* TEXT BOOK EXAMPLE
 
