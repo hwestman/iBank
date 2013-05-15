3d50463906727201b594e0103354f64e032a3301
Lance N. Solomon <lance.solomon@griffithuni.edu.au>
2013-05-15 16:56:23 +1000

    WOO stored procedures are rocking!

--- sql/create_procedures.sql
+++ sql/create_procedures.sql
@@ -13,23 +13,28 @@
     DECLARE
     BEGIN
     END
+    	
+    	//// CREATE USER PROCUEDRE ////
+    	VARIABLE loginUserID NUMBER;
+		EXEC createUser('5','35 Serafina Drive','Queensland', 'Lance N. Solomon', 'shanaye', 4, '0421008826', :loginUserID);
+		PRINT loginUserID;
+		/
     
-    
-    
+		//// CREATING ACCOUNT PROCEDURE ////
     	variable accountNumber NUMBER;
     	EXEC :accountNumber := createAccount(1,4,'',4);
     	PRINT accountNumber;
     	/
     	
     	variable receiptNumber NUMBER;
-    	EXEC :receiptNumber := transferFunds(18283004,18280330,'Enjoy',15000);
+    	EXEC transferFunds(18283004,18280330,'Enjoy',300.25, :receiptNumber);
     	PRINT receiptNumber;
     	/
 */
 
 SET SQLBLANKLINES ON
 
-EXEC createUser('5','35 Serafina Drive','Queensland', 'Lance N. Solomon', 'shanaye', 4, '0421008826');
+/*///////////////////////////// CREATING A USER PROCEDURE /////////////////////////////*/
 
 CREATE OR REPLACE PROCEDURE createUser (
 	suburbID IN VARCHAR2,
@@ -37,7 +42,7 @@ CREATE OR REPLACE PROCEDURE createUser (
 	county IN VARCHAR2,
 	fullName IN VARCHAR2,  
 	pword IN VARCHAR2, 
-	priv IN INTEGER, 
+	priv IN NUMBER, 
 	contactNumber IN VARCHAR2,
 	loginID OUT NUMBER
 )
@@ -52,13 +57,50 @@ BEGIN
 		VALUES ('', fullName, addressID, pword, priv, contactNumber)
 		RETURNING login_id INTO loginID;
 	COMMIT;
+	RETURN;
 END;
 /
-select * from SYS.USER_ERRORS;
 
+/*///////////////////////////// TRANSFER FUNDS PROCEDURE /////////////////////////////*/
+CREATE OR REPLACE PROCEDURE transferFunds (
+	accountFrom IN NUMBER,
+	accountTo IN NUMBER,
+	message IN VARCHAR2,
+	money IN NUMBER,	
+	receiptNumber OUT NUMBER
+)
+AS
+fromAmount NUMBER;
+BEGIN	
+		SELECT balance INTO fromAmount FROM ibankAccount WHERE accountFrom = account_number;
 
+		fromAmount := fromAmount - money;
+		
+		IF fromAmount < 0 THEN
+			RETURN;
+		ELSE
+			INSERT INTO ibankTransaction
+				(transaction_id, from_account, to_account, memo, amount)
+				VALUES ('', accountFrom, accountTo, message, money)
+				RETURNING transaction_id INTO receiptNumber;
+			
+			UPDATE ibankAccount
+				SET balance = balance - money WHERE accountFrom = ibankAccount.account_number;
+			
+			UPDATE ibankAccount
+				SET balance = balance + money WHERE accountTo = ibankAccount.account_number;
+			
+			IF receiptNumber > 0 THEN
+				COMMIT;
+				RETURN;
+			ELSE
+				ROLLBACK;
+			END IF;
+		END IF;
+END;
+/
 
-
+/*///////////////////////////// CREATING AN ACCOUNT FUNCTION /////////////////////////////*/
 CREATE OR REPLACE FUNCTION createAccount (
 	staffID IN NUMBER,
 	loginID IN NUMBER,
@@ -77,69 +119,3 @@ BEGIN
 			RETURN accountNumber;
 END;
 /
-
-
-CREATE OR REPLACE FUNCTION transferFunds (
-	accountFrom IN NUMBER,
-	accountTo IN NUMBER,
-	message IN VARCHAR2,
-	money IN NUMBER	
-)
-RETURN NUMBER
-AS
-fromAmount NUMBER;
-receiptNumber NUMBER;
-BEGIN	
-		SELECT balance FROM ibankAccount WHERE accountFrom = account_number;
-		RETURNING balance INTO fromAmount;
-
-		fromAmount := fromAmount - money;
-		
-		IF fromAmount < 0 THEN
-			RETURN 0;
-		ELSE
-			INSERT INTO ibankTransaction
-				(transaction_id, from_account, to_account, memo, amount)
-				VALUES ('', accountFrom, accountTo, message, money)
-				RETURNING transaction_id INTO receiptNumber;
-			
-			UPDATE ibankAccount
-				SET balance = balance - money WHERE accountFrom = ibankAccount.account_number;
-			
-			UPDATE ibankAccount
-				SET balance = balance + money WHERE accountTo = ibankAccount.account_number;
-			
-			IF receiptNumber > 0 THEN
-				COMMIT;
-				RETURN receiptNumber;
-			ELSE
-				ROLLBACK;
-				RETURN 0;
-			END IF;
-		END IF;
-END;
-/
-
-select * from SYS.USER_ERRORS;
-
-/* TEXT BOOK EXAMPLE
-
-CREATE PROCEDURE remove_emp (employee_id NUMBER) AS
-tot_emps NUMBER;
-BEGIN
-   DELETE FROM employees
-   WHERE employees.employee_id = remove_emp.employee_id;
-tot_emps := tot_emps - 1;
-END;
-/
-
-
-CREATE PROCEDURE Addlnventory (
-	IN book_isbn CHAR(lO),
-	IN addedQty INTEGER)
-
-UPDATE Books
-	SET qty_in_stock = qtyjn_stock + addedQty
-	WHERE bookjsbn = isbn
-	
-END TEXT BOOK EXAMPLE */
